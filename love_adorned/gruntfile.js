7a9a0af96faca3f50598c781e107744c4e8d2ec9
Aleksandr Rzhevskyi <rjevska.an@gmail.com>
2020-01-20 13:33:52 +0200

    love adorned

--- love_adorned/gruntfile.js
+++ love_adorned/gruntfile.js
@@ -1,9 +1,5 @@
 module.exports = function(grunt) {
 
-    require('load-grunt-tasks')(grunt);
-
-    var PathConfig = require('./grunt-settings.js');
-
     grunt.initConfig({
 
         pkg: grunt.file.readJSON('package.json'),
@@ -63,26 +59,6 @@ module.exports = function(grunt) {
                 files: ['js/**/*'],
                 tasks: ['uglify']
             }
-        },
-
-        'sftp-deploy': {
-          build: {
-            auth: {
-              host: '<%= config.sftpServer %>',
-              port: '<%= config.sftpPort %>',
-              authKey: {
-                        "username": "<%= config.sftpLogin %>",
-                        "password": "<%= config.sftpPas %>"
-                      }
-            },
-            cache: 'sftpCache.json',
-            src: 'css',
-            dest: '<%= config.sftpDestination %>',
-            //exclusions: ['/path/to/source/folder/**/.DS_Store', '/path/to/source/folder/**/Thumbs.db', 'dist/tmp'],
-            serverSep: '/',
-            concurrency: 4,
-            progress: true
-          }
         }
     });
 
@@ -91,9 +67,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
-
-    // upload to server
-    grunt.registerTask('sftp', ['sftp-deploy']);
 
     // Default task
     grunt.registerTask('default', 'build');
